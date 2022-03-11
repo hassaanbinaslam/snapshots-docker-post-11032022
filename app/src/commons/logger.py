10ee2d24f410dc38f40bdffb1285acbe5dd59d81
hassaan-kavtech <hassaan@kavmail.net>
2022-03-11 13:11:53 +0500

    snapshot-01

--- app/src/commons/logger.py
+++ app/src/commons/logger.py
@@ -4,13 +4,8 @@
 import logging
 import os
 
-
-if not os.path.exists("logs"):
-    os.makedirs("logs")
-
 logformat = "%(levelname)s %(asctime)s - %(message)s"
 filename = "logfile.log"
-
 
 # Setting the config of the log object
 logging.basicConfig(
